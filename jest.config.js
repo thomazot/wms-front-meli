2e519967b45b9dd71c44fa4a7d09cf8da9a1a8e8
Thomaz Takashi Oda Toyama <thomazot@gmail.com>
2021-11-18 00:25:20 -0300

    fix: update jest.config

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,9 @@ module.exports = {
     '!src/types/**/*.d.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
