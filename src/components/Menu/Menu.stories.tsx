9feab7b3258b6922d571f61c95237ddf5c5ad119
Thomaz Takashi Oda Toyama <thomazot@gmail.com>
2021-11-20 20:14:54 -0300

    fix: adjust decorate in Menu stories

--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -1,10 +1,17 @@
 import { Story, Meta } from '@storybook/react'
-import Menu from '.'
+import Menu, { MenuProps } from './Menu'
 import items from './Menu.mock'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen'
+  }
 } as Meta
 
-export const Default: Story = () => <Menu items={items} />
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />
+
+Default.args = {
+  items
+}
